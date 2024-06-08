0bc7f77186ceb3da584887e35b8c40fea9885c04
Luisa Moraes <luisamoraes221@gmail.com>
2024-06-08 16:22:19 -0300

    layout home page

--- gerencia_de_ponto/lib/main.dart
+++ gerencia_de_ponto/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:gerencia_de_ponto/pages/home_page.dart';
 import 'package:gerencia_de_ponto/pages/login_page.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
